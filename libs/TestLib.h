9f8ff7cb39353addd1ddb62263bd5f538f964d88
Python3xsssss <biathloner2@mail.ru>
2021-03-26 17:14:34 +0300

    Initial commit

---
+++ libs/TestLib.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#ifndef TESTLIB_H
+#define TESTLIB_H
+
+void goFwd(int maxPower)
+{
+	motor[motorB] = maxPower;
+	motor[motorC] = maxPower;
+}
+
+int newColorCheck()
+{
+	return SensorValue[S1];
+}
+
+#endif //TESTLIB_H
---
+++ src/MainTestLib.c
@@ -0,0 +1,9 @@
+#include "TestLib.h"
+
+
+task main()
+{
+	int maxPower = 80;
+	int color = newColorCheck();
+	goFwd(maxPower);
+}
